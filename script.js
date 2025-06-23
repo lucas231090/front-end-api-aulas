6fb7d91efcd3a55ab5b4507020fa31a9f6a0ab87
Lucas <souza.lucasj@gmail.com>
2025-06-23 16:45:34 -0300

    ajustando

---
+++ script.js
@@ -0,0 +1,9 @@
+const API_URL = "http://localhost:3000/aulas";
+
+const formCriar = document.getElementById("form-criar");
+const formBuscar = document.getElementById("form-buscar");
+const formEditar = document.getElementById("form-editar");
+const formDeletar = document.getElementById("form-deletar");
+
+const resultadoBusca = document.getElementById("resultado-busca");
+const listaAulas = document.querySelector(".aula-lista");
